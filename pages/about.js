d4841741139b7806e786f5dca6e1602e469c693c
Chris Oloff <christian.oloff@gmail.com>
2017-10-27 16:11:07 +0200

    Added static site generation

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout'
+
+export default () => {
+
+  return (
+    <Layout title="About">
+      <div>Basic Login Setup, but via server-side rendering.</div>
+    </Layout>
+  )
+  
+}
