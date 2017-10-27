ded5638b7c8a3fe747ea35fbe799011c64751a06
Chris Oloff <christian.oloff@gmail.com>
2017-10-27 16:15:14 +0200

    added generated output to git

---
+++ _next/4b0ddc6c-23f3-4d62-af7b-e156805520c8/page/about/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([5],{188:function(e,t,u){e.exports=u(189)},189:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var r=u(6),l=n(r),i=u(100),o=n(i);t.default=function(){return l.default.createElement(o.default,{title:"About"},l.default.createElement("div",null,"Basic Login Setup, but via server-side rendering."))}}},[188]);
+            return { page: comp.default }
+          })
+        
---
+++ _next/4b0ddc6c-23f3-4d62-af7b-e156805520c8/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([4],{207:function(e,t,l){e.exports=l(208)},208:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(6),o=u(n),r=l(100),d=u(r);t.default=function(){return o.default.createElement(d.default,{title:"Hello!"},o.default.createElement("div",null,"Hello, World..."))}}},[207]);
+            return { page: comp.default }
+          })
+        
